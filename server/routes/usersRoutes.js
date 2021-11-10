4ac84179e6fdfa7e88f97f4058f3e9be11178f53
Sergio Garcia <sergio.gl86@gmail.com>
2021-11-10 20:44:40 +0100

    Added in user controller /users/login

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const { validate } = require("express-validation");
-const { addUser } = require("../controller/usersController");
+const {
+  addUser,
+  getusers,
+  loginUser,
+} = require("../controller/usersController");
 const userValidation = require("../schemas/userSchema");
 
 const router = express.Router();
 
 router.post("/register", validate(userValidation), addUser);
+router.get("/all", getusers);
+router.post("/login", validate(userValidation), loginUser);
 
 module.exports = router;
