68ad6dd553327dc65aabf5e2207013fb62d2af7f
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-11 10:39:44 +0100

    Added test for delete with wrong id on seriesController

--- server/controller/seriesController.test.js
+++ server/controller/seriesController.test.js
@@ -1,5 +1,5 @@
 const Serie = require("../../database/models/serie");
-const { getSeries } = require("./seriesController");
+const { getSeries, deletedSerie } = require("./seriesController");
 
 jest.mock("../../database/models/serie");
 
@@ -45,6 +45,29 @@ describe("Given a Serie function", () => {
       await getSeries(null, res, next);
 
       expect(Serie.find).toHaveBeenCalled();
+    });
+  });
+
+  describe("When a Id arrives and there isn't in the database", () => {
+    test("Then it should return a error with a 404 code", async () => {
+      Serie.findByIdAndDelete = jest.fn().mockResolvedValue(null);
+      const idSerie = 21;
+      const req = {
+        params: {
+          idSerie,
+        },
+      };
+      const next = jest.fn();
+      const error = {
+        code: 404,
+        message: "Serie no encontrada",
+      };
+
+      await deletedSerie(req, null, next);
+
+      expect(next).toHaveBeenCalled();
+      expect(next.mock.calls[0][0]).toHaveProperty("message", error.message);
+      expect(next.mock.calls[0][0]).toHaveProperty("code", error.code);
     });
   });
 });
